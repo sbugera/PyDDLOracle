4f1788d0b59d24d7102d6bb50cd3cf55e0a91336
sbugera <30780377+sbugera@users.noreply.github.com>
2023-09-23 23:06:37 +0200

    store DDL into files

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 import pandas as pd
 import argparse
 import configparser
+import re
+import os
 import sql_queries as sql
 
 pd.options.mode.chained_assignment = None  # type: ignore # default='warn'
@@ -10,34 +12,16 @@ arg_parser = argparse.ArgumentParser(description='Generate DDL scripts for Oracl
 arg_parser.add_argument('--schema_name', '-s', type=str, help='DB schema name for which DDL scripts need to be generated')
 args = arg_parser.parse_args()
 
+conf_con = configparser.ConfigParser()
+conf_con.read('config_con.ini')
+
 conf = configparser.ConfigParser()
 conf.read('config.ini')
-
-conf_format = configparser.ConfigParser()
-conf_format.read('config.formatter.ini')
-
-db_username = conf['database']['username']
-db_password = conf['database']['password']
-db_host = conf['database']['host']
-db_port = conf['database']['port']
-db_service_name = conf['database']['service_name']
-connection_string = f"oracle+cx_oracle://{db_username}:{db_password}@{db_host}:{db_port}/?service_name={db_service_name}"
-engine = create_engine(connection_string, arraysize=1000)
-
-schema_name = db_username.upper()
-if args.schema_name:
-    schema_name = args.schema_name
-
-df_tables = pd.read_sql_query(sql.sql_tables, engine, params={
-                              'schema_name': schema_name})
-df_all_tab_columns = pd.read_sql_query(sql.sql_tab_columns, engine, params={
-                                       'schema_name': schema_name})
-
 
 def get_case_formatted(str, config_name_for_upper):
     if str != str.upper():
         return f'"{str}"'
-    if conf_format["case"][config_name_for_upper] == "uppercase":
+    if conf["case"][config_name_for_upper] == "uppercase":
         return str.upper()
     else:
         return str.lower()
@@ -83,10 +67,10 @@ def get_column_name(col_column_name, max_column_name_length):
 
 
 def get_indentation():
-    if conf_format["alignments"]["indentation-mode"] == "tab":
+    if conf["alignments"]["indentation-mode"] == "tab":
         indentation = "\t"
     else:
-        indentation = " "*int(conf_format["alignments"]["indentation-size"])
+        indentation = " "*int(conf["alignments"]["indentation-size"])
     return indentation
 
 
@@ -143,13 +127,72 @@ def generate_table_ddl(table, columns):
     return ddl
 
 
+def get_entity_name(template_name, object_type, object_owner, object_name):
+    pattern = r'\{(.*?)\}'
+    matches = re.findall(pattern, template_name)
+    entity_name = template_name
+    for match in matches:
+        case_function = str.lower
+        if match == match.upper():
+            case_function = str.upper
+        
+        if match.lower() == 'object_type':
+            entity_name = entity_name.replace(case_function('{object_type}'), case_function(object_type))
+        elif match.lower() == 'object_owner':
+            entity_name = entity_name.replace(case_function('{object_owner}'), case_function(object_owner))
+        elif match.lower() == 'object_name':
+            entity_name = entity_name.replace(case_function('{object_name}'), case_function(object_name))
+    return entity_name
+
+
+def get_file_name(object_type, object_owner, object_name):
+    file_name_template = conf['filename'][object_type]
+    file_name = get_entity_name(file_name_template, object_type, object_owner, object_name)
+    return file_name
+
+
+def get_file_directory(object_type, object_owner, object_name):
+    file_directory_template = conf['directory'][object_type]
+    file_directory = get_entity_name(file_directory_template, object_type, object_owner, object_name)
+    return file_directory
+    
+    
+def store_ddl_into_file(file_content, file_directory, file_name):
+    if not os.path.exists(file_directory):
+        os.makedirs(file_directory)
+    with open(os.path.join(file_directory, file_name), 'w') as file:
+        file.write(file_content)
+
+
+db_username = conf_con['database']['username']
+db_password = conf_con['database']['password']
+db_host = conf_con['database']['host']
+db_port = conf_con['database']['port']
+db_service_name = conf_con['database']['service_name']
+connection_string = f"oracle+cx_oracle://{db_username}:{db_password}@{db_host}:{db_port}/?service_name={db_service_name}"
+engine = create_engine(connection_string, arraysize=1000)
+
+schema_name = db_username.upper()
+if args.schema_name:
+    schema_name = args.schema_name
+
+df_tables = pd.read_sql_query(sql.sql_tables, engine, params={
+                              'schema_name': schema_name})
+df_all_tab_columns = pd.read_sql_query(sql.sql_tab_columns, engine, params={
+                                       'schema_name': schema_name})
+
 for table in df_tables.itertuples():
     print("----------------------------")
     print(table.table_name)
     print("----------------------------")
     df_tab_columns = df_all_tab_columns[df_all_tab_columns["table_name"] == table.table_name]
     table_ddl = generate_table_ddl(table, df_tab_columns)
-    print(table_ddl)
+    # print(table_ddl)
+    # table_ddl = "test content"
+    file_directory = get_file_directory('table', schema_name, table.table_name)
+    file_name = get_file_name('table', schema_name, table.table_name)
+    print(f"Stored in {file_directory}/{file_name}")
+    store_ddl_into_file(table_ddl, file_directory, file_name)
 
 
 engine.dispose()
