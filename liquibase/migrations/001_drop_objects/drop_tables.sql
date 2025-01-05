3fb5460e3d17b029d7e9554ae11e6894c8089964
Sergii Bugera <sergey.bugera@gmail.com>
2025-01-05 20:56:00 +0100

    add SQLPlus execution

--- liquibase/migrations/001_drop_objects/drop_tables.sql
+++ liquibase/migrations/001_drop_objects/drop_tables.sql
@@ -1,6 +1,11 @@
+PROMPT Dropping tables
+
 BEGIN
-    FOR i IN (SELECT table_name FROM user_tables) LOOP
-        EXECUTE IMMEDIATE 'DROP TABLE ' || i.table_name || ' CASCADE CONSTRAINTS PURGE';
+    FOR i IN (SELECT table_name FROM user_tables WHERE table_name NOT IN ('DATABASECHANGELOG', 'DATABASECHANGELOGLOCK')) LOOP
+        dbms_output.put_line('Table "' || i.table_name || '"');
+        EXECUTE IMMEDIATE 'DROP TABLE "' || i.table_name || '" CASCADE CONSTRAINTS PURGE';
     END LOOP;
 END;
 /
+
+PROMPT Tables dropped
