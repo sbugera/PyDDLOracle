8df1c125e180d98bb1ef7c7ff080b3d4c23e1333
Sergii Bugera <sergey.bugera@gmail.com>
2025-10-24 15:36:23 +0200

    Reduce number of attributes in Index

--- pyddl_oracle/index.py
+++ pyddl_oracle/index.py
@@ -8,37 +8,7 @@ from pyddl_oracle.utils import get_case_formatted, get_object_name, get_prompt
 class Index:
     """Oracle database index with DDL generation."""
     def __init__(self, index_row, index_columns):
-        self.owner = index_row.owner
-        self.index_name = index_row.index_name
-        self.index_type = index_row.index_type
-        self.table_owner = index_row.table_owner
-        self.table_name = index_row.table_name
-        self.uniqueness = index_row.uniqueness
-        self.compression = index_row.compression
-        self.prefix_length = index_row.prefix_length
-        self.tablespace_name = index_row.tablespace_name
-        self.ini_trans = index_row.ini_trans
-        self.max_trans = index_row.max_trans
-        self.initial_extent = index_row.initial_extent
-        self.next_extent = index_row.next_extent
-        self.min_extents = index_row.min_extents
-        self.max_extents = index_row.max_extents
-        self.pct_increase = index_row.pct_increase
-        self.pct_threshold = index_row.pct_threshold
-        self.include_column = index_row.include_column
-        self.freelists = index_row.freelists
-        self.freelist_groups = index_row.freelist_groups
-        self.pct_free = index_row.pct_free
-        self.logging = index_row.logging
-        self.instances = index_row.instances
-        self.partitioned = index_row.partitioned
-        self.buffer_pool = index_row.buffer_pool
-        self.flash_cache = index_row.flash_cache
-        self.cell_flash_cache = index_row.cell_flash_cache
-        self.visibility = index_row.visibility
-        self.monitoring = index_row.monitoring
-        self.degree = index_row.degree
-        self.instances = index_row.instances
+        self.row = index_row
         self.index_columns = index_columns
 
     def get_index(self):
@@ -48,14 +18,16 @@ class Index:
         )
 
         index_type = ""
-        if self.index_type == "BITMAP":
+        if self.row.index_type == "BITMAP":
             index_type += get_case_formatted(" BITMAP", "keyword")
-        if self.uniqueness == "UNIQUE":
+        if self.row.uniqueness == "UNIQUE":
             index_type += get_case_formatted(" UNIQUE", "keyword")
 
-        index_name = get_object_name(self.owner, self.index_name, "identifier")
+        index_name = get_object_name(
+            self.row.owner, self.row.index_name, "identifier"
+        )
         table_name = get_object_name(
-            self.table_owner, self.table_name, "identifier"
+            self.row.table_owner, self.row.table_name, "identifier"
         )
 
         index_columns = ""
@@ -76,69 +48,70 @@ class Index:
 
         logging = ""
         if c.conf["storage"]["logging"] == "yes":
-            if self.logging == "YES":
+            if self.row.logging == "YES":
                 logging = get_case_formatted("\nLOGGING", "keyword")
-            elif self.logging == "NO":
+            elif self.row.logging == "NO":
                 logging = get_case_formatted("\nNOLOGGING", "keyword")
         index += logging
 
         if c.conf["storage"]["storage"] == "with_storage":
             index += get_full_storage(
                 "",
-                self.tablespace_name,
-                self.pct_free,
-                self.ini_trans,
-                self.max_trans,
-                self.min_extents,
-                self.max_extents,
-                self.pct_increase,
-                self.buffer_pool,
-                self.flash_cache,
-                self.cell_flash_cache,
-                self.initial_extent,
-                self.next_extent,
-                self.partitioned,
+                self.row.tablespace_name,
+                self.row.pct_free,
+                self.row.ini_trans,
+                self.row.max_trans,
+                self.row.min_extents,
+                self.row.max_extents,
+                self.row.pct_increase,
+                self.row.buffer_pool,
+                self.row.flash_cache,
+                self.row.cell_flash_cache,
+                self.row.initial_extent,
+                self.row.next_extent,
+                self.row.partitioned,
             )
         elif (
             c.conf["storage"]["storage"] == "only_tablespace"
-            and self.partitioned != "YES"
+            and self.row.partitioned != "YES"
         ):
             statement = get_case_formatted("\nTABLESPACE <:1>", "keyword")
             index += statement.replace(
-                "<:1>", get_case_formatted(self.tablespace_name, "identifier")
+                "<:1>",
+                get_case_formatted(self.row.tablespace_name, "identifier"),
             )
 
         if c.conf["storage"]["compression"] == "yes":
-            if self.compression == "ENABLED":
+            if self.row.compression == "ENABLED":
                 index += get_case_formatted(
-                    f"\nCOMPRESS {int(self.prefix_length)}", "keyword"
+                    f"\nCOMPRESS {int(self.row.prefix_length)}", "keyword"
                 )
-            elif self.compression != "DISABLED":
+            elif self.row.compression != "DISABLED":
                 index += get_case_formatted(
-                    f"\nCOMPRESS {self.compression}", "keyword"
+                    f"\nCOMPRESS {self.row.compression}", "keyword"
                 )
 
         local = ""
-        if self.partitioned == "YES":
+        if self.row.partitioned == "YES":
             local = get_case_formatted("\nLOCAL", "keyword")
         index += local
 
-        if self.visibility == "INVISIBLE":
+        if self.row.visibility == "INVISIBLE":
             index += get_case_formatted("\nINVISIBLE", "keyword")
 
-        if int(self.degree) > 1:
+        if int(self.row.degree) > 1:
             index += get_case_formatted(
-                f"\nPARALLEL ( DEGREE {int(self.degree)}"
-                f" INSTANCES {self.instances} )",
+                f"\nPARALLEL ( DEGREE {int(self.row.degree)}"
+                f" INSTANCES {self.row.instances} )",
                 "keyword",
             )
 
-        if self.index_type == "NORMAL/REV":
+        if self.row.index_type == "NORMAL/REV":
             index += get_case_formatted("\nREVERSE", "keyword")
 
         index += ";\n\n"
 
-        if self.monitoring == "YES":
+        if self.row.monitoring == "YES":
             statement = get_case_formatted(
                 "ALTER INDEX <:1>\n  MONITORING USAGE;\n\n", "keyword"
             )
